330afb5f87a48bf3c7769c3b12916c783597ce11
eXtremeSHOK <admin@extremeshok.com>
2020-09-15 03:22:48 +0200

    fix folders then files

--- rootfs/xshok-fix-permissions.sh
+++ rootfs/xshok-fix-permissions.sh
@@ -7,18 +7,28 @@
 ## enable case insensitve matching
 shopt -s nocaseglob
 
-###### Fix vhost permissions ######
+###### Fix vhost permissions : folders ######
 if [ -d "/var/www/vhosts" ] ; then
 
   while IFS= read -r -d '' my_vhost_dir; do
-    echo "Fixing vhost permissions"
+    echo "Fixing vhost permissions : folders"
     if [ -d "${my_vhost_dir}/html" ] ; then
-      find "${my_vhost_dir}/html" -type f -exec chmod 0664 {} \;
       find "${my_vhost_dir}/html" -type d -exec chmod 0775 {} \;
     fi
     if [ -d "${my_vhost_dir}/certs" ] ; then
       chown -R nobody:nogroup "${my_vhost_dir}/certs"
       chmod -R 640 "${my_vhost_dir}/certs"
+    fi
+  done < <(find "/var/www/vhosts" -mindepth 1 -maxdepth 1 -type d -print0)  #dirs
+fi
+
+###### Fix vhost permissions : files ######
+if [ -d "/var/www/vhosts" ] ; then
+
+  while IFS= read -r -d '' my_vhost_dir; do
+    echo "Fixing vhost permissions : files"
+    if [ -d "${my_vhost_dir}/html" ] ; then
+      find "${my_vhost_dir}/html" -type f -exec chmod 0664 {} \;
     fi
   done < <(find "/var/www/vhosts" -mindepth 1 -maxdepth 1 -type d -print0)  #dirs
 fi
